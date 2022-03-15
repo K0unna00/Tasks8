20b4d9c0d7b5645c20ad9097beee7ed3bc0d1237
K0unna00 <78pf4jz@code.edu.az>
2022-03-16 00:07:09 +0400

    Tasks8

---
+++ Tasks8/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace Tasks8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ///////////1ci sual
+            ///////1ci usul
+            //Console.WriteLine("Stringi daxil et : ");
+            //string str = Console.ReadLine();
+            //foreach(char i in str)
+            //{
+            //    if (i == '1' || i == '2' || i == '3' || i == '4' || i == '5' || i == '6' || i == '7' || i == '8' || i == '9' || i == '0')
+            //    {
+            //        Console.WriteLine("Reqem var");
+            //        break;
+            //    }
+            //}
+            ///////2ci usul
+            //Console.WriteLine("Stringi daxil et : ");
+            //string str = Console.ReadLine();
+            //foreach(char i in str)
+            //{
+            //    if (Char.IsDigit(i)) Console.WriteLine("True");
+            //    break;
+            //}
+
+            ///////////2ci sual
+            //Console.WriteLine("Stringi daxil et : ");
+            //string str= Console.ReadLine();
+            //for (int i = 0; i < str.Length-1; i++)
+            //{
+            //    if (str[i] == 'a' && str[i + 1] == 'l')
+            //    {
+            //        Console.WriteLine("Var");
+            //        break;
+            //    }
+            //}
+
+
+            ////////3cu sual
+            //Console.WriteLine("int daxil et : ");
+            //int num = int.Parse(Console.ReadLine());
+            //int sum = 0;
+            //int count = 0;
+            //while (num != 0)
+            //{
+            //    sum += num % 10;
+            //    num /= 10;
+            //    count++;
+            //}
+            //Console.WriteLine((double)sum / count);
+
+
+
+            ////////4cu sual
+            //Console.WriteLine("Sozlerin sayini yazin");
+            //int num=int.Parse(Console.ReadLine());
+            //string[] arr= new string[num];
+            //for (int j = 0; j < num; j++)
+            //{
+            //    Console.WriteLine($"{j+1}. heddi elave et");
+            //    arr[j] = Console.ReadLine();
+            //}
+            //int count = 0;
+            //foreach(string item in arr)
+            //{
+            //    for (int i = 0; i <item.Length; i++)
+            //    {
+            //        if (Char.IsDigit(item[i]))
+            //        {
+            //            count++;
+            //            break;
+            //        }
+            //    }
+            //}
+            //Console.WriteLine(count);
+        }
+    }
+}
